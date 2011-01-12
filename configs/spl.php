9fb9060d453cff6c95a40db4d43a2ad1bb0b26cf
Enrique Urzúa <urzuae@gmail.com>
2011-01-11 19:12:33 -0600

    Add system files

---
+++ configs/spl.php
@@ -0,0 +1,8 @@
+<?php
+$_dbuname = 'root';
+$_dbpass = 'redsox';
+$_dbname = 'spl';
+$_site_title = 'Sistema Prospeccion Licencias';
+$_theme = "Verde claro";
+$_licenses = "100";
+$array_colors=array('#696969','#800000','#745896');
