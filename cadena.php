96843a3fa11cb8cae530379fd1d5a7f06760b6bb
Enrique Urzúa <urzuae@gmail.com>
2011-01-14 11:38:50 -0600

    Update remote methods

--- cadena.php
+++ cadena.php
@@ -15,52 +15,47 @@ $db = connect($_dbhost, $_dbuname, $_dbpass, $_dbname);
 $params = array();
 if (isset($_POST['data']))
 {
-  $data = json_decode($_POST['data']);
-    foreach ($data as $key=>$value)
-    {
-      $params[]=$value;
-    }
+  //$data = json_decode($_POST['data']);
+  $data = unserialize($_POST['data']);
+  foreach ($data as $key=>$value)
+    $params[]=$value;
+  
+  $gid = $params[0];
+  $super_val = $params[1];
+  $user = $params[2];
+  $password = $params[3];
+  $name = $params[4];
+  $email = $params[5];
+  
+  if ($db->sql_numrows($db->sql_query("SELECT name FROM users WHERE user='$user'")) > 0)
+    $error = "<br>Ese usuario ya esta registrado en el sistema, intenta otro nombre de usuario";
+  else
+  {
+    $password = strtoupper($password);
+    $res = $db->sql_query("INSERT INTO users (`user`, `name`, `gid`, `super`, `password`,`email`) VALUES('$user', '$name', '$gid', '$super_val', PASSWORD('$password'), '$email')")
+        or die("No se pudo agregar el usuario ".print_r($db->sql_error()));
     
-    $gid = $params[0];
-    $super_val = $params[1];
-    $user = $params[2];
-    $password = $params[3];
-    $name = $params[4];
-    $email = $params[5];
+    $uid = $db->sql_nextid();
     
-    print_r($params);
-    if ($db->sql_numrows($db->sql_query("SELECT name FROM users WHERE user='$user'")) > 0)
-      $error = "<br>Ese usuario ya esta registrado en el sistema, intenta otro nombre de usuario";
-    else
+    //ahora seteamos la configuración por default (copiando la de uid=0 en la db)
+    $result = $db->sql_query("SELECT * FROM users_configs WHERE uid='0' LIMIT 1")
+    or die("Error al cargar la configuracion por default");
+    $row = $db->sql_fetchrow($result);
+    //el primer valor sera el uid, así ke lo kambiamos por el nuevo
+    $values = "'$uid'";
+    $i = 1;
+    while (isset($row[$i]))
     {
-      $password = strtoupper($password);
-      $res = $db->sql_query("INSERT INTO users (`user`, `name`, `gid`, `super`, `password`,`email`) VALUES('$user', '$name', '$gid', '$super_val', PASSWORD('$password'), '$email')")
-          or die("No se pudo agregar el usuario ".print_r($db->sql_error()));
-          
-          print_r($res);
-      $uid = $db->sql_nextid();
-      
-      print_r($uid);
-      //ahora seteamos la configuración por default (copiando la de uid=0 en la db)
-      $result = $db->sql_query("SELECT * FROM users_configs WHERE uid='0' LIMIT 1")
-      or die("Error al cargar la configuracion por default");
-      $row = $db->sql_fetchrow($result);
-      //el primer valor sera el uid, así ke lo kambiamos por el nuevo
-      $values = "'$uid'";
-      $i = 1;
-      while (isset($row[$i]))
-      {
-          $values .= ", '".$row[$i++]."'";
-      }
-      $sql = "INSERT INTO users_configs VALUES($values)";
-      $db->sql_query($sql) or die("Error al crear configuración por default");
+        $values .= ", '".$row[$i++]."'";
     }
-
-  echo 1;
+    $sql = "INSERT INTO users_configs VALUES($values)";
+    $db->sql_query($sql) or die("Error al crear configuración por default");
+  }
+  return true;
 }
 else
 {
-  echo 0;
+  return false;
 }
 
 function connect($_dbhost, $_dbuname, $_dbpass, $_dbname)
