a73058169e075c1ab82e0ed2853422425319e1de
Enrique Urzúa <urzuae@gmail.com>
2011-01-14 13:26:15 -0600

    Relocate conection files for remote access

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,22 +1,19 @@
 <?php
 
-$_dbhost = 'localhost';
-$_dbuname = 'root';
-$_dbpass = 'redsox';
-$_dbname = 'spl';
-$db = connect($_dbhost, $_dbuname, $_dbpass, $_dbname);
+include("../configs/spl.php");
+include("../includes/db/mysql.php");
+include("../includes/db/db.php");
 
-# cadena.php en servidor remoto
-/*$data = array(0 => "47,78,1,0,asdas",
-    1 => "47,78,2,0,asdasd",
-    2=>"47,78,3,0,454545",
-    3 => "47,78,4,0,asda@adsd.com",
-    4=>"47,78,5,0,asdasd");*/
+global $db;
+
 $params = array();
-if (isset($_POST['data']))
+if (isset($_REQUEST['data']))
 {
   //$data = json_decode($_POST['data']);
-  $data = unserialize($_POST['data']);
+  $data = unserialize($_REQUEST['data']);
+  
+  print_r($data);
+  
   foreach ($data as $key=>$value)
     $params[]=$value;
   
@@ -26,6 +23,8 @@ if (isset($_POST['data']))
   $password = $params[3];
   $name = $params[4];
   $email = $params[5];
+  
+  print_r($params);
   
   if ($db->sql_numrows($db->sql_query("SELECT name FROM users WHERE user='$user'")) > 0)
     $error = "<br>Ese usuario ya esta registrado en el sistema, intenta otro nombre de usuario";
@@ -58,11 +57,4 @@ else
   return false;
 }
 
-function connect($_dbhost, $_dbuname, $_dbpass, $_dbname)
-{
-  include("includes/db/mysql.php");
-  $_dbtype = 'MySQL';
-  $db = new sql_db($_dbhost, $_dbuname, $_dbpass, $_dbname, false);
-  return $db;
-}
 ?>
