1e3fe54ad5286739f4f0f0ea282f3c9e0e0fe382
Enrique Urzúa <urzuae@gmail.com>
2011-01-17 14:08:25 -0600

    Graphic reports

--- configs/spl.php
+++ configs/spl.php
@@ -2,7 +2,7 @@
 $_dbuname = 'root';
 $_dbpass = 'redsox';
 $_dbname = 'spl';
-$_site_title = 'Sistema Prospeccion Licencias';
-$_theme = "Verde claro";
+$_site_title = 'Sistema de Prospección de Licencias';
+$_theme = "Azul claro";
 $_licenses = "100";
 $array_colors=array('#696969','#800000','#745896');
