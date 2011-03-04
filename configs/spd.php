91a5414b0f621172b6bc9613b2b921a8bdc033a5
Enrique Urzúa <urzuae@gmail.com>
2011-03-04 17:50:42 -0600

    Create zend-base project

--- configs/spd.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_dbuname = 'root';
-$_dbpass = 'mysql_pwd!';
-$_dbname = 'spd';
-$_site_title = 'Sistema de Prospección de Distribuidores';
-$_theme = "Azul claro";
-$_licenses = "100";
-$array_colors=array('#696969','#800000','#745896');
--- configs/spl.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_dbuname = 'root';
-$_dbpass = 'redsox';
-$_dbname = 'spl';
-$_site_title = 'Sistema de Prospección de Licencias';
-$_theme = "Azul claro";
-$_licenses = "100";
-$array_colors=array('#696969','#800000','#745896');
